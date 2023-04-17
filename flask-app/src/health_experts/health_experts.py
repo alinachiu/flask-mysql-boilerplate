af6a5eba4535c33bba8ebf6862b207282a46d8e5
Alina Chiu <chiu.a@northeastern.edu>
2023-04-17 14:57:51 -0400

    finish health experts blueprint and add one preliminary test for each route

--- flask-app/src/health_experts/health_experts.py
+++ flask-app/src/health_experts/health_experts.py
@@ -10,7 +10,7 @@ health_experts = Blueprint('health_experts', __name__)
 def get_posts():
     cursor = db.get_db().cursor()
     cursor.execute('select n.title, h.first_name,\
-        h.last_name from NutritionalAdvice n join healthexpert h on n.author_id = h.expert_id')
+        h.last_name from NutritionalAdvice n join HealthExpert h on n.author_id = h.expert_id')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -26,17 +26,17 @@ def get_posts():
 def add_new_post():
     # collecting data from the request object
     data = request.json
-    current_app.logger.info(data)
 
     # extracting the variables
     title = data['post_title']
     body = data['post_body']
+    author = data['author_id']
 
     # constructing the query
-    query = 'insert into products (title, text_body) values("'
+    query = 'insert into NutritionalAdvice (title, text_body, author_id) values("'
     query += title + '", "'
-    query += body + '")'
-    current_app.logger.info(query)
+    query += body + '", '
+    query += str(author) + ')'
 
     # executing and committing the insert statement
     cursor = db.get_db().cursor()
@@ -48,40 +48,107 @@ def add_new_post():
 # Return a list of all nutritional advice posts from a particular health expert
 @health_experts.route('/nutritionaladviceposts/<author_id>', methods=['GET'])
 def get_author_posts(author_id):
-    return "TODO"
+    cursor = db.get_db().cursor()
+    cursor.execute('select n.title, h.first_name,\
+        h.last_name from NutritionalAdvice n join HealthExpert h on n.author_id = h.expert_id\
+                   where n.author_id =' + str(author_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 # Return all the detailed information for a particular nutritional advice post
-# including their title, text body, author, and timestamp
+# including their title, text body, author name, and timestamp
 @health_experts.route('/nutritionaladviceposts/<author_id>/<post_id>', methods=['GET'])
 def get_specific_post(author_id, post_id):
-    return "TODO"
+    cursor = db.get_db().cursor()
+    cursor.execute('select n.title, n.text_body, h.first_name, h.last_name, n.date_posted\
+                   from NutritionalAdvice n join HealthExpert h on n.author_id = h.expert_id\
+                   where n.post_id =' + str(post_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 # Update the text body of the post from an expert
 @health_experts.route('/nutritionaladviceposts/<author_id>/<post_id>', methods=['PUT'])
 def update_post_body(author_id, post_id):
-    return "TODO"
+    # collecting data from the request object
+    data = request.json
+
+    # extracting the text body
+    body = data['new_post_body']
+
+    # constructing the query
+    query = 'update NutritionalAdvice set text_body = "'
+    query += body + '" where post_id = '
+    query += post_id + ' and author_id = ' + author_id
+
+    # executing and committing the update statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return "Success!"
 
 # Remove a nutritional advice post
 @health_experts.route('/nutritionaladviceposts/<author_id>/<post_id>', methods=['DELETE'])
 def remove_post(author_id, post_id):
-    return "TODO"
+    # constructing the query
+    query = 'delete from NutritionalAdvice where post_id = ' + post_id + ' and author_id = ' + author_id
 
-# Expert recommends a new recipe
-@health_experts.route('/recommendedrecipes/<expert_id>/<recipe_id>', methods=['POST'])
-def recommend_recipe(expert_id, recipe_id):
-    return "TODO"
+    # executing and committing the delete statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
 
-# Expert unrecommends a recipe by removing it from their recommended list
-@health_experts.route('/recommendedrecipes/<expert_id>/<recipe_id>', methods=['DELETE'])
-def unrecommend_recipe(expert_id, recipe_id):
-    return "TODO"
+    return "Success!"
 
 # Expert recommends a new meal plan for a specific user
-@health_experts.route('/mealplans/<expert_id>/<user_id>', method=['POST'])
+@health_experts.route('/mealplans/<expert_id>/<user_id>', methods=['POST'])
 def recommend_meal_plan(expert_id, user_id):
-    return "TODO"
+    # collecting data from the request object
+    data = request.json
+
+    # extracting the variables
+    start_date = data['start_date']
+
+    # constructing the query
+    set_key_query = 'set @new_key := (SELECT meal_plan_id FROM MealPlan ORDER BY meal_plan_id DESC LIMIT 1)'
+    select_key_query = 'select @new_key'
+    query = 'insert into MealPlan (meal_plan_id, dietitian_id, user_id, start_date) values (@new_key + 1, '
+    query += expert_id + ', '
+    query += user_id + ', "'
+    query += start_date + '")'
+
+    # executing and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(set_key_query)
+    cursor.execute(select_key_query)
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return "Success!"
 
 # Expert unrecommends a user's meal plan from their list of recommended meal plans
-@health_experts.route('/mealplans/<user_id>/<meal_plan_id>', method=['DELETE'])
+@health_experts.route('/mealplans/<user_id>/<meal_plan_id>', methods=['DELETE'])
 def unrecommend_meal_plan(user_id, meal_plan_id):
-    return "TODO"
+    # constructing the query
+    query = 'delete from MealPlan where user_id = ' + user_id + ' and meal_plan_id = ' + meal_plan_id
+
+    # executing and committing the delete statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return "Success!"
